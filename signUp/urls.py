16d290247f2f6d9a127f02f712a50a244248b74a
Aditya <adityasjc31@gmail.com>
2023-04-30 19:00:04 +0530

    deploy 1

--- signUp/urls.py
+++ signUp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from signUp.views import *
 urlpatterns = [
     path('',SignUp,name="signUp"),
-    path('otp',getOtp,name="otp"),
+    path('otp',getOtp,name="otp"),# type: ignore   
     # path('mail',email,name="mail")
     # path('email',email,name="email")
 ]
